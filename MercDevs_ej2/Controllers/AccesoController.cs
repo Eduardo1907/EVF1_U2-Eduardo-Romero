3ffe5d2068c7a28957a2609841e06a3d713ed516
eduardo romero <eduardo.romero.fernandez@cftmail.cl>
2024-06-07 20:02:21 -0400

    first commit

---
+++ MercDevs_ej2/Controllers/AccesoController.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Applogin.Models;
+using MercDevs_ej2.Models;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.DotNet.Scaffolding.Shared.Project;
+
+namespace MercDevs_ej2.Controllers
+{
+    public class AccesoController : Controller
+    {
+        private readonly MercydevsEjercicio2Context _mercydevsEjercicio2Context;
+        public AccesoController(MercydevsEjercicio2Context mercydevsEjercicio2Context) 
+        {
+            _mercydevsEjercicio2Context = mercydevsEjercicio2Context;
+
+        }
+        [HttpGet]
+        public IActionResult Ingreso()
+        {
+            return View();
+        }
+        [HttpPost]
+        public IActionResult Ingreso(Ingreso modelo)
+        {
+
+
+            return RedirectToAction("Index", "Home") ;
+        }
+
+        
+    }
+}
---
+++ MercDevs_ej2/Models/Ingreso.cs
@@ -0,0 +1,9 @@
+﻿namespace Applogin.Models
+{
+    public class Ingreso
+    {
+        public int IdIngreso { get; set; }
+        public string Correo { get; set; }
+        public string Clave { get; set; }
+    }
+}
